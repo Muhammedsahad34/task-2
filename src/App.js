132165bf953780e489e632fc522431c288e23e07
Muhammed Sahad <muhammedsahad34@gmail.com>
2023-12-20 14:33:51 +0530

    Aside created successfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Aside from './Components/Aside/Aside';
 import Header from './Components/Header/Header';
 
 function App() {
   return (
     <>
     <Header/>
+    <Aside/>
     </>
   );
 }
